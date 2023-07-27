c4ffd4b94fc63bc45086b8bb4cc404c60b985d6d
chiinwy <chiinwy@gmail.com>
2023-07-27 14:42:53 +0200

    Challenge 2

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -71,3 +71,41 @@ ORDER BY
 LIMIT 3;
 
 -- Challenge 2
+
+create temporary table tabla_temporal as
+
+SELECT
+    subquery.author_id AS 'AUTHOR ID',
+    SUM(titles.advance) + SUM(subquery.aggregated_royalties) AS total_profits
+FROM
+    (
+        SELECT
+            subquery.title_id,
+            subquery.author_id,
+            SUM(subquery.sales_royalty) AS aggregated_royalties
+        FROM
+            (
+                SELECT
+                    titles.title_id,
+                    titleauthor.au_id AS author_id,
+                    titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS sales_royalty
+                FROM
+                    titles
+                JOIN
+                    sales ON titles.title_id = sales.title_id
+                JOIN
+                    titleauthor ON titles.title_id = titleauthor.title_id
+            ) AS subquery
+        GROUP BY
+            subquery.title_id,
+            subquery.author_id
+    ) AS subquery
+JOIN
+    titles ON subquery.title_id = titles.title_id
+GROUP BY
+    subquery.author_id
+ORDER BY
+    total_profits DESC
+LIMIT 3;
+
+-- Challenge 3
