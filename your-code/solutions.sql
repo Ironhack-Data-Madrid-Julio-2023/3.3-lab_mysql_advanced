a5ef460e8929252fe66c773b22be309fe1218a8b
Demian Figueiredo <demian.c.figueiredo@gmail.com>
2023-07-27 17:23:43 +0200

    soluciones completas  clase 3.3

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -1,4 +1,4 @@
--- CHALLLENGE # 1 - Most Profiting Authors
+-- CHALLENGE 1 -- CHALLENGE 1
 SELECT
     t.royalty AS 'Royalty',
     ta.royaltyper AS 'Royalty Percentage'
@@ -32,17 +32,7 @@ JOIN
 JOIN
     sales s ON ta.title_id = s.title_id;
     
-CREATE TEMPORARY TABLE temp_salesroyalty AS
-SELECT
-    ta.title_id AS 'Title ID',
-    ta.au_id AS 'Author ID',
-    (t.price * s.qty * t.royalty * ta.royaltyper / 10000) AS 'Sales Royalty'
-FROM
-    titleauthor ta
-JOIN
-    titles t ON ta.title_id = t.title_id
-JOIN
-    sales s ON ta.title_id = s.title_id;
+
     
     SELECT * from temp_salesroyalty;
     SELECT
@@ -54,15 +44,7 @@ FROM
 GROUP BY
     `Title ID`, `Author ID`;
     
-    CREATE TEMPORARY TABLE temp_Totalsalesroyalty AS
-    SELECT
-    `Title ID`,
-    `Author ID`,
-    SUM(`Sales Royalty`) AS 'Total Sales Royalty'
-		FROM
-			temp_salesroyalty
-		GROUP BY
-			`Title ID`, `Author ID`;
+   
             
             SELECT * FROM  temp_Totalsalesroyalty;
             
@@ -78,7 +60,7 @@ JOIN
 GROUP BY
 	ta.au_id, ta.title_id;
     
-    SELECT
+SELECT
     ta.au_id AS 'Author ID',
     SUM(tt.`Total Sales Royalty` + t.advance) AS 'Profit'
 FROM
@@ -93,7 +75,38 @@ ORDER BY
     `Profit` DESC
 LIMIT 3;
 
--- CHALLENGE # 2 - Alternative Solution
+CREATE TEMPORARY TABLE most_profiting_authors AS
+SELECT
+    ta.au_id AS 'Author ID',
+    SUM(tt.`Total Sales Royalty` + t.advance) AS 'Profit'
+FROM
+    titleauthor ta
+JOIN
+    titles t ON ta.title_id = t.title_id
+JOIN
+    temp_Totalsalesroyalty tt ON ta.title_id = tt.`Title ID`
+GROUP BY
+	ta.au_id, ta.title_id;
+    
+    
+-- CHALLENGE 3 -- CHALLENGE 3
+
+
+CREATE TABLE most_profiting_author AS 
+SELECT
+    ta.au_id AS 'Author ID',
+    SUM(ttr.`Total Sales Royalty` + t.advance) AS 'Total Profit'
+FROM
+    titleauthor ta
+JOIN
+    titles t ON ta.title_id = t.title_id
+JOIN
+    temp_Totalsalesroyalty ttr ON ta.title_id = ttr.`Title ID`
+GROUP BY
+    ta.au_id;
+    
+    
+    -- CHALLENGE 2 -- CHALLENGE 2
 
 SELECT
     t.royalty AS 'Royalty',
@@ -202,4 +215,4 @@ GROUP BY
 ORDER BY
     `Profit` DESC
 LIMIT 3;
-    
+
